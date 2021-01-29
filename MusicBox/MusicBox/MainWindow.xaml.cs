66eac0823d862efbffbc590f2467eab14d56bb9f
Nnamdi Ekweariri <ekwearirinnamdi@gmail.com>
2021-01-29 21:27:42 +0100

    Added some events code doesn't compile

--- MusicBox/MusicBox/MainWindow.xaml.cs
+++ MusicBox/MusicBox/MainWindow.xaml.cs
@@ -24,5 +24,45 @@ namespace MusicBox
         {
             InitializeComponent();
         }
+
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // set tooltip visibility
+            if (tog_button.IsChecked==true)
+            {
+                tt_home.Visibility = Visibility.Collapsed;
+                tt_User.Visibility = Visibility.Collapsed;
+                tt_messages.Visibility = Visibility.Collapsed;
+                tt_maps.Visibility = Visibility.Collapsed;
+                tt_settings.Visibility = Visibility.Collapsed;
+                tt_logout.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                tt_home.Visibility = Visibility.Visible;
+                tt_User.Visibility = Visibility.Visible;
+                tt_messages.Visibility = Visibility.Visible;
+                tt_maps.Visibility = Visibility.Visible;
+                tt_settings.Visibility = Visibility.Visible;
+                tt_logout.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void tog_button_Unchecked(object sender, RoutedEventArgs e)
+        {
+            img_bg.Opacity = 1;
+
+        }
+
+        private void tog_button_Checked(object sender, RoutedEventArgs e)
+        {
+            img_bg.Opacity = 0.3;
+        }
+
+        private void bg_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            tog_button.IsChecked = false;
+
+        }
     }
 }
